8c6c1f5d3674cc14e31c24a3a9ae2d351e1b8ed1
bartick <bartickmaiti1998@gmail.com>
2023-05-29 09:57:53 +0530

    fix: fixed some bugs in repl

--- repl/repl.go
+++ repl/repl.go
@@ -13,12 +13,15 @@ import (
 	"github.com/anilang-official/AniLang/parser"
 )
 
-const PROMPT = ">> "
+var PROMPT = ">> "
 
 // Start starts the REPL
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
+
+	var BRACECOUNTER int = 0
+	var line string = ""
 
 	for {
 		fmt.Print(PROMPT)
@@ -26,26 +29,32 @@ func Start(in io.Reader, out io.Writer) {
 		if !scanned {
 			return
 		}
-		line := strings.TrimSpace(scanner.Text())
 
-		if line == ".exit" {
+		if BRACECOUNTER > 0 {
+			line = line + "\n" + strings.TrimSpace(scanner.Text())
+		} else {
+			line = strings.TrimSpace(scanner.Text())
+		}
+
+		if line == ".exit" && BRACECOUNTER == 0 {
 			fmt.Println("Bye 👋")
 			return
 		}
 
 		if line[len(line)-1] == '{' {
-			// multiline input
-			for {
-				fmt.Print("... ")
-				scanned := scanner.Scan()
-				if !scanned {
-					return
-				}
-				line += "\n" + strings.TrimSpace(scanner.Text())
-				if line[len(line)-1] == '}' {
-					break
-				}
+			BRACECOUNTER++
+			PROMPT = "... "
+		}
+
+		if line[len(line)-1] == '}' {
+			BRACECOUNTER--
+			if BRACECOUNTER == 0 {
+				PROMPT = ">> "
 			}
+		}
+
+		if BRACECOUNTER > 0 {
+			continue
 		}
 
 		l := lexer.New(line)
@@ -85,10 +94,15 @@ func ReplFile(filename string, out io.Writer) {
 
 	program := p.ParseProgram()
 	if len(p.Errors()) != 0 {
-		printParserErrors(io.Writer(os.Stderr), p.Errors())
+		printParserErrors(out, p.Errors())
 		return
 	}
 
-	evaluator.Eval(program, env)
+	evaluated := evaluator.Eval(program, env)
+	if evaluated != nil {
+		if evaluated.Type() == object.ERROR_OBJ {
+			io.WriteString(out, evaluated.Inspect())
+		}
+	}
 	io.WriteString(out, "\n")
 }
